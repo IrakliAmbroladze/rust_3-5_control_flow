f723f2b4007b8a0c90138173fbd589da5f2e0830
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-30 07:41:55 +0400

    if statement notnequal zero

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
     let number = 3;
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    if number != 0 {
+        println!("number was somthing other than zero");
     }
 }
